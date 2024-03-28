30842983a7b50c82a6c655ce1ea2814ba8ce5b8f
Orvius <Okunev.Aleksander@mail.ru>
2024-03-29 01:30:29 +0300

    feat: location button, maximum object scaling

--- src/components/LocationMarker/LocationMarker.tsx
+++ src/components/LocationMarker/LocationMarker.tsx
@@ -1,25 +1,51 @@
-import { useState } from "react";
+import "./LocationMarker.css";
 
+import locationFalse from "../../assets/images/Location_False.svg";
+import locationTrue from "../../assets/images/Location_True.svg";
+
+import { useState } from "react";
 import { LatLngExpression } from "leaflet";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 
 const LocationMarker: React.FC = () => {
   const [position, setPosition] = useState<LatLngExpression | null>(null);
+  const [active, setActive] = useState<boolean>(false);
 
   const map = useMapEvents({
-    click() {
-      map.locate()
-    },
     locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
+      if (active) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getMaxZoom());
+      }
     },
-  })
-  
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
+  });
+
+  const handleLocateButtonClick = () => {
+    setActive(true);
+      map.locate();
+  };
+
+  return (
+    <>
+      {position !== null && (
+        <Marker position={position}>
+          <Popup>You are here</Popup>
+        </Marker>
+      )}
+      <button
+        className={`location-button ${active ? "active" : ""}`}
+        onClick={handleLocateButtonClick}
+      >
+        <img
+          src={active ? locationTrue : locationFalse}
+          alt={
+            active
+              ? "Your geolocation is turned on"
+              : "Your geolocation is turned off"
+          }
+        />
+      </button>
+    </>
   );
 };
 
